98d48343299c96d809d39798619eff4bae24157f
YoungHedgehog <92035751+ELJarzynski@users.noreply.github.com>
2022-10-13 00:15:19 +0200

    Update Zadanka.py

--- lab01/Zadanka.py
+++ lab01/Zadanka.py
@@ -4,6 +4,8 @@ def imie(a , b):
 a="jan"
 b="Kowalski"
 print(imie(a,b))
+
+
 #zad2
 def foo(pierwsza,nazwisko):
     pom = pierwsza[0].title()
@@ -12,6 +14,7 @@ pierwsza = "jan"
 nazwisko = "brzeczyszczykiewicz"
 print(foo(pierwsza, nazwisko))
 #zad3
+
 def rok(a,b,c):
     d=str(a)+str(b)
     d=int(d)
@@ -20,32 +23,42 @@ a="22"
 b="33"
 c=44
 print(rok(a,b,c))
-#zad4
+
+
+#zad 4
 def fun(imie,nazwisko,foo):
     return foo(imie,nazwisko)
 print(fun("kamil","jarzyski",foo))
 
-#zad5
+
+#zad 5
 def funny(a,b):
     if a>0 and b>0 and b!=0:
         return(a/b)
 print(funny(22,0))
 
 
+#zad 6
+suma=0
+while(suma<100):
+    a=int(input("podaj cyfre do zsumowania: "))
+    suma+=a
+    print(suma)
+
 
 #zad 7
-lista=[2,1,3,7]
-def fuga(lista):
+lista=[6,9,8,7]
+def figaro(lista):
     return tuple(lista)
-a=fuga(lista)
+a=figaro(lista)
 print(a)
 print(type(a))
 print(lista)
 print(type(lista))
 
+
 #zad8
 list=[]
-
 n=int(input("ile wartosci w tablicy: "))
 
 i=0
@@ -57,6 +70,8 @@ while i<n:
 list=tuple(list)
 print(list)
 
+
+#zad 9
 def dobry(a):
     dobry={1: "Fajniedziałek",
            2: "Ftorek",
@@ -67,15 +82,16 @@ def dobry(a):
            7: "nedzielnika"}
     return dobry[a]
 print(dobry(3))
+
+
 #zad 10
 a="balwan"
-def palindrom(string):
-    a=len(string)-1
-    for i in range(len(string)):
-        if string[i] != string[a-i]:
+def palindrom(napis):
+    a=len(napis)-1
+    for i in range(len(napis)):
+        if napis[i] != napis[a-i]:
             return False
     return True
-
 print(palindrom(a))
 
 
